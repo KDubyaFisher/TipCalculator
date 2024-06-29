f650eb102f0fbcfe99ad3da6fa2a892da7397cb4
Kolton Fisher <fisher.kolton@gmail.com>
2024-06-29 17:01:38 -0500

    feat: implement basic functionality for Tip Calculator
    - Add greeting message
    - Prompt user for the total bill amount
    - Prompt user for the tip percentage
    - Prompt user for the number of people to split the bill
    - Calculate and format the amount each person should pay
    - Display the amount each person should pay

--- main.py
+++ main.py
@@ -6,3 +6,18 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+print("Welcome to Tip Calculator!")
+
+total_bill = float(input("What is your total bill? $"))
+
+tip_percent = int(input("How much would you like to tip? "))
+
+num_people = int(input("How many people to split the bill? "))
+
+price_per_person = (total_bill / num_people) * ((tip_percent / 100) + 1)
+
+price_per_person = "{:.2f}".format(price_per_person)
+
+print(f"Each person should pay: ${price_per_person}")
+
+
